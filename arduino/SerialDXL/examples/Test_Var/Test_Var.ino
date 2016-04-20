c96c87128d3ccc369e7c1387a3c23076c4e429ca
Rodrigo Muñoz <rorro.mr@gmail.com>
2016-04-20 12:03:27 -0300

    Test var

--- arduino/SerialDXL/examples/Test_Var/Test_Var.ino
+++ arduino/SerialDXL/examples/Test_Var/Test_Var.ino
@@ -1,16 +1,20 @@
 #include <SerialDXL.h>
 
-MMap::UInt8NV foo_nv(0, 5, MMap::Access::RW, 10, 60, 13);
+MMap::UInt8NV foo_nv(0, 0, MMap::Access::RW, 10, 60, 13);
 
 MMap::UInt8 foo(1, MMap::Access::RW, 10, 200, 25);
 MMap::UInt8 bar(2, MMap::Access::RW, 10, 200, 50);
 
 
 uint8_t mmap_buf[] = {0x00,0x00,0x00,0x00};
+MMap::VariablePtr var_list[3];
+
+MMap::MMap mem_map(3);
 
 
 void setup() {
   Serial.begin(9600);
+  mem_map.init(mmap_buf, var_list);
   foo_nv.save();// Save default
 }
 
