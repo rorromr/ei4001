d2132be521846bb5db9ff382d31d8a7f52d5e246
Rodrigo Muñoz <rorro.mr@gmail.com>
2016-04-22 22:26:52 -0300

    Test receive

--- test/rs485_test.py
+++ test/rs485_test.py
@@ -12,19 +12,20 @@ def calc_checksum(msg):
     chksum = ( ~chksum ) % 256
     return chksum
 
-ser = serial.Serial('/dev/ttyUSB1', baudrate = 9600, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', baudrate = 9600, timeout=1)
 # [0xFF, 0xFF, ID, Length, Instruction, Parameters, Checksum]
 
-command = int(sys.argv[1])
-print 'Command: ' + str(command)
+# command = int(sys.argv[1])
+# print 'Command: ' + str(command)
 
 # ID, Length, Instruction, Parameters
-msg = [0x01, 0x03, 0x01, command]
-packet = [0xFF,0xFF] + msg + [calc_checksum(msg)]
-packet_hex = ['%02x,'%i for i in packet]
-print packet_hex
-print packet
+# msg = [0x01, 0x03, 0x01, command]
+# packet = [0xFF,0xFF] + msg + [calc_checksum(msg)]
+# packet_hex = ['%02x,'%i for i in packet]
+# print packet_hex
+# print packet
 
-data = array('B', packet).tostring() # packetStr = ''.join([chr(byte) for byte in packet])
+# data = array('B', packet).tostring() # packetStr = ''.join([chr(byte) for byte in packet])
 
-ser.write(data)
+while True:
+	print ser.readline()
